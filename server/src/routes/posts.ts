18b6ca1a95f2bb5a8a0d9ca1973f597d9f7d3f2f
Aditya Sinha <sinhadityaforever@gmail.com>
2022-06-08 12:39:24 +0530

    Added update route

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { createPost, getPosts } from '../controllers/posts';
+import { createPost, getPosts, updatePost } from '../controllers/posts';
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
+router.patch('/:id', updatePost);
 export default router;
