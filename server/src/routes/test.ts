5c5e72ed0e4698422b7619ddb94d713e90248efb
Aditya Sinha <sinhadityaforever@gmail.com>
2022-06-20 20:58:44 +0530

    Updated test code

--- server/src/routes/test.ts
+++ server/src/routes/test.ts
@@ -4,4 +4,12 @@ const router = express.Router();
 router.get('/', (req: any, res: any) => {
 	res.status(200).json({ message: 'Test Success' });
 });
+router.post('/', (req: any, res: any) => {
+	const { email, password } = req.body;
+	try {
+		res.status(400).json({ email, password });
+	} catch (error) {
+		res.json(400).json({ message: 'something went wrong' });
+	}
+});
 export default router;
